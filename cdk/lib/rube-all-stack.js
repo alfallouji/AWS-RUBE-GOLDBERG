d6d218e01fe37c70357a447046131509fbadc857
EC2 Default User <ec2-user@ip-172-31-3-49.eu-central-1.compute.internal>
2020-09-26 13:56:32 +0000

    Adde cfn policy

--- cdk/lib/rube-all-stack.js
+++ cdk/lib/rube-all-stack.js
@@ -78,6 +78,7 @@ class RubeAllStack extends cdk.Stack {
     // allow instance to communicate with secrets manager & ssm (for debug purposes if needed)
     role.addManagedPolicy(iam.ManagedPolicy.fromAwsManagedPolicyName(props.instance.roleManagedPolicyName1));
     role.addManagedPolicy(iam.ManagedPolicy.fromAwsManagedPolicyName(props.instance.roleManagedPolicyName2));    
+    role.addManagedPolicy(iam.ManagedPolicy.fromAwsManagedPolicyName(props.instance.roleManagedPolicyName3));
     role.addManagedPolicy(iam.ManagedPolicy.fromAwsManagedPolicyName(props.iam.ssmManagedPolicyName));
     
     // User data that will deploy the application on the instance
